042d72183e9130ebf625758d2080738dee9591a6
Staszek Cieslicki <s.cieslicki@clearcode.cc>
2016-03-15 17:06:55 +0100

    tutorial-2

--- new_task.php
+++ new_task.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 include 'config/config.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection(
         $host = $config['host'],
@@ -29,16 +30,18 @@ $connection = new AMQPStreamConnection(
 
 $channel = $connection->channel();
 
-$channel->queue_declare('hallo', false, false, false, false);
+$channel->queue_declare('task_queue', false, true, false, false);
 
-echo ' [*] Waiting for messages. To exit press CTRL-C', "\n";
-
-$callback = function($msg) {
-    echo "[x] Received ", $msg->body, "\n";
-};
-
-$channel->basic_consume('hallo', '', false, true, false, false, $callback);
-
-while(count($channel->callbacks)) {
-    $channel->wait();
+$data = implode(' ', array_slice($argv, 1));
+if(empty($data)) {
+    $data = "Hello World!";
 }
+$msg = new AMQPMessage($data,
+    array('delivery_mode' => 2)
+);
+$channel->basic_publish($msg, '', 'task_queue');
+
+echo " [x] Sent ", $data, "\n";
+
+$channel->close();
+$connection->close();
--- worker.php
+++ worker.php
@@ -5,7 +5,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 include 'config/config.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection(
         $host = $config['host'],
@@ -30,12 +29,23 @@ $connection = new AMQPStreamConnection(
 
 $channel = $connection->channel();
 
-$channel->queue_declare('hallo', false, false, false, false);
+$channel->queue_declare('task_queue', false, true, false, false);
 
-$msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hallo');
+echo ' [*] Waiting for messages. To exit press CTRL-C', "\n";
 
-echo " [x] Sent 'Hello World!'\n";
+$callback = function($msg) {
+    echo " [x] Received ", $msg->body, "\n";
+    sleep(substr_count($msg->body, '.'));
+    echo " [x] Done", "\n";
+    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+};
+
+$channel->basic_qos(null, 1, null);
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
 
 $channel->close();
 $connection->close();
